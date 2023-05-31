a95b9ab634cd0ab64e9b9e8cbaaa35c9bfe1fa4a
Frank Nkuyahaga <hagafrank@gmail.com>
2023-05-31 14:08:59 +0300

    Enclose ID in quotes

--- models/m_product_loan_view.sql
+++ models/m_product_loan_view.sql
@@ -3,7 +3,7 @@ WITH source AS (
         "ID" as id,
         lp.ENCODEDKEY as external_id,
         PRODUCTNAME AS name,
-        SUBSTRING(id, 0, 3) AS short_name,
+        SUBSTRING("ID", 0, 3) AS short_name,
         CURRENCYCODE AS currency_code,
         DEFAULTLOANAMOUNT as principal_amount,
         MINLOANAMOUNT as min_principal_amount,
