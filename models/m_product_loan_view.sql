4e7c6e1a1af6b5d4edab27faacafb8cbc1be9c48
Frank Nkuyahaga <hagafrank@gmail.com>
2023-05-31 14:42:59 +0300

    Use a uuid instead -- use the right function name

--- models/m_product_loan_view.sql
+++ models/m_product_loan_view.sql
@@ -3,7 +3,7 @@ WITH source AS (
         "ID" as id,
         lp.ENCODEDKEY as external_id,
         PRODUCTNAME AS name,
-        SUBSTRING(uuid_generate_v4(), 0, 3) AS short_name,
+        SUBSTRING(UUID(), 0, 3) AS short_name,
         CURRENCYCODE AS currency_code,
         DEFAULTLOANAMOUNT as principal_amount,
         MINLOANAMOUNT as min_principal_amount,
