093ec9225c7708c3ca4d33930eadbcf8a980a926
tabrez-88 <113360881+tabrez-88@users.noreply.github.com>
2023-06-20 22:14:16 +0800

    handle missing constraints

--- models/m_group_view.sql
+++ models/m_group_view.sql
@@ -7,13 +7,13 @@
 SELECT 
     ROW_NUMBER() OVER () as id,
     {{ decode_base64("encodedkey") }} as external_id,
-    office.id as office_id,
+    COALESCE(office.id, 1) as office_id,
     staff.id as staff_id,
     "creationdate" as submittedon_date,
     {{ decode_base64("groupname") }} as display_name,
     "ID" as account_no,
     300 as status_enum, -- default value for status_enum in your PostgreSQL table
-    1 as level_id -- assuming some default value for level_id, replace as appropriate
+    2 as level_id -- level_name Group
 FROM {{ ref('final_group') }}
 LEFT JOIN {{ ref('m_office_view') }} as office
 ON {{ decode_base64("assignedbranchkey") }} = office.external_id
